f487bcad94be0ac1875aede935a98418e90e9d23
aibad <aibadkhan812@gmail.com>
2025-01-02 19:48:23 +0530

    added cors policy

--- backend/index.js
+++ backend/index.js
@@ -370,7 +370,14 @@ const app = express();
 const port = process.env.PORT||3001;
 dotenv.config();
 // Set up multer to handle file uploads
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+  origin: "https://your-frontend-url.com", // Replace with your front-end URL
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+
+app.use(cors(corsOptions));
 app.use(express.json())
 // const storage = multer.diskStorage({
 //   destination: (req, file, cb) => {
