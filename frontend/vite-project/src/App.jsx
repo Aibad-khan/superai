4827829048de17bfd26e9ce87a46bbf8c7b1b209
aibad <aibadkhan812@gmail.com>
2025-01-02 13:18:17 +0530

    first commit

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+
+import './App.css'
+import CsvQueryComponent from './components/Homepage';
+import Homepage2 from './components/Homepage2';
+// import NaturalLanguageToSQL from './components/text_to_sql';
+// import { DuckDBProvider } from '@duckdb/react-duckdb';
+// import DatabaseComponent from './components/duck';
+// import DuckDBQuery from './components/Homepage';
+
+function App() {
+  return (
+    // <DuckDBProvider>
+    //   <DatabaseComponent />
+    // </DuckDBProvider>
+    // <CsvQueryComponent/>
+    // <NaturalLanguageToSQL/>
+    <Homepage2/>
+
+  );
+}
+export default App;
---
+++ frontend/vite-project/src/components/text_to_sql.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+// import axios from "axios";
+
+// const NaturalLanguageToSQL = () => {
+  
+    
+
+    
+
+//   return (
+//     <div style={{ padding: "20px" }}>
+      
+//     </div>
+//   );
+// };
+
+// export default NaturalLanguageToSQL;
